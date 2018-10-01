0abed8e731ef043463af8907e7c2a969ab5f37cb
INW-TECH\ricardo.trindade <ricardo.trindade@innowave.tech>
2018-10-01 16:08:00 +0100

    step 3

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import env from '@env';
+
+const App = () => console.log({env}) || (
+    <div>
+        <h1>Hello, world!!!!!!!!!!!</h1>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
